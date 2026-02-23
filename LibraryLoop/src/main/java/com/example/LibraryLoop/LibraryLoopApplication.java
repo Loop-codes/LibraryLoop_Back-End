9117fd526252cdd2f3e57b357c398e7e9a78113e
Gustavo <140917585+Guzitos@users.noreply.github.com>
2026-02-23 19:02:51 -0300

    Initial commit

---
+++ LibraryLoop/src/main/java/com/example/LibraryLoop/LibraryLoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibraryLoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryLoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryLoopApplication.class, args);
+	}
+
+}
---
+++ LibraryLoop/src/test/java/com/example/LibraryLoop/LibraryLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryLoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryLoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
